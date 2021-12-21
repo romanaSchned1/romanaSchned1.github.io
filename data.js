23cfc98b821a960bb82dff3fd57b3b31816b8a36
romanaSchned1 <romschana@gmail.com>
2021-12-21 14:41:49 +0100

    index.html modified

--- data.js
+++ data.js
@@ -1,14 +1,2 @@
 
-//picure of see //name von see //1.see Tiefe //2.Seehoehe //3.ob Stillgewaesser //4.Bundeslaender
-let seen=[
-    ["./img/see1.jpg", "Seewaldsee ", "14m ", "1.132m ", "Stillgeweasser ", "Vorarlberg"],
-    ["./img/see2.jpg","Lunzer See", "34m", "608m", "Stillgewaesser", "Niederoesterreich"],
-    ["./img/see3.jpg","Steirersee", "21m", "1.445m", "Stillgewaesser", "Steiermark"],
-    ["./img/see4.jpg","Blindsee", "25m", "1.093m", "Stillgewaesser", "Tirol"],
-    ["./img/see5.jpg","Langbathseen", "33m", "664m", "Stillgewaesser", "Oberoesterreich"],
-    ["./img/see6.jpg","Turnersee", "13m", "481m", "Stillgewaesser", "Kaernten"],
-    ["./img/see7.jpg","Almsee", "5m", "589m", "Stillgewaesser", "Oberoesterreich"],
-    ["./img/see8.jpg","Leopoldsteinersee", "31m", "628m", "Stillgewaesser", "Steiermarks"],
-    ["./img/see9.jpg","Tieber See", "10m",  "449m", "Stillgewaesser", "Steiermark"],
-    ["./img/see10.jpg","Achensee", "133m",  "929m", "Stillgewaesser", "Tirol"],
-];
+//picure of seen //name von seen //1.seen Tiefe //2.seenhoehe //3.ob Stillgewaesser //4.Bundeslaender
--- scriptfile.js
+++ scriptfile.js
@@ -3,6 +3,19 @@ let fromHTML = document.getElementById("textOutput");
 
 let codeka = ""; 
 
+let seenn=[
+    ["./img/seen1.jpg"],
+    ["./img/seen2.jpg"],
+    ["./img/seen3.jpg"],
+    ["./img/seen4.jpg"],
+    ["./img/seen5.jpg"],
+    ["./img/seen6.jpg"],
+    ["./img/seen7.jpg"],
+    ["./img/seen8.jpg"],
+    ["./img/seen9.jpg"],
+    ["./img/seen10.jpg"],
+];
+
 
 for(let i = 0; i < seen.length; i++){
     codeka += "<div>"
