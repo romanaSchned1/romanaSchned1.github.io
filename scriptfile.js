f2f9cbc86df5e1a488ff4d3c87dd8274567a5850
romanaSchned1 <romschana@gmail.com>
2021-12-21 14:44:07 +0100

    index.html modified

--- scriptfile.js
+++ scriptfile.js
@@ -19,15 +19,15 @@ let seen=[
 
 for(let i = 0; i < seen.length; i++){
     codeka += "<div>"
-    for(let j = 0; j < seen[i].length; j++){
-        if(j == 0){
-            codeka += `<img class="boxPic" src="${seen[i][j]}"></img>`;
-        }
-        else{
+    //for(let j = 0; j < seen[i].length; j++){
+     //   if(j == 0){
+            codeka += `<img class="boxPic" src="${seen[i]}"></img>`;
+       // }
+       /* else{
             codeka += `<p class="box" >${seen[i][j]}</p>`;
         }
         
-    }  
+    }  */
     codeka += "</div>" 
 }
 
